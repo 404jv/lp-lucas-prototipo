b3bde529acb72334f2dc7c774306eb21a7e96991
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2025-10-30 15:55:59 -0300

    update photos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Feedback from "@/components/Feedback"
-import Image from "next/image";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
+import About from "@/components/About";
 
 
 export default function Home() {
